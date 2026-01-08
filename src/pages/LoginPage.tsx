8844e7a4a506c0ddc244de48eedcca6bc2a45b99
buboyskie26 <justinesirios15@gmail.com>
2026-01-08 20:07:09 +0800

    Added registration and fix bugs v2

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -80,7 +80,7 @@ export default function LoginPage() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
-              className="mt-1 block w-full border border-gray-00 rounded-lg px-3 py-2 text-gray-00 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="mt-1 block w-full border border-gray-700 rounded-lg px-3 py-2 text-gray-00 focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="••••••••"
             />
           </div>
